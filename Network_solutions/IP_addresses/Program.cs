66f9c8393067eb2554b6d032e28f433622b4b855
narekye <yegoryan.narek@gmail.com>
2017-04-08 12:11:59 -0600

    :octocat: simple ip addresses show :octocat:

--- Network_solutions/IP_addresses/Program.cs
+++ Network_solutions/IP_addresses/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Net;
 namespace IP_addresses
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            IPHostEntry host1 = Dns.GetHostEntry("Google.com");
+            Console.WriteLine(host1.HostName);
+            foreach (IPAddress ip in host1.AddressList)
+            {
+                Console.WriteLine(ip.ToString());
+            }
+            // Delay !!
+            Console.Read();
         }
     }
 }
