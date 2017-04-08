7509e0cc184c229ce21d5e7b4e4dcd1d92bab7b4
narekye <yegoryan.narek@gmail.com>
2017-04-08 16:12:18 -0600

    :octocat: server-client connection example :octocat:

---
+++ Network_solutions/Client_Side_Sockets/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_Side_Sockets
+{
+    class Program
+    {
+        private static int prot = 8005;
+        static IPAddress ippoint = IPAddress.Parse("127.0.0.1");
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
